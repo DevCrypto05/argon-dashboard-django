ec3b4252395296ab3619a9da4182bdf345d0825b
Arletto <ramarijaonaaimearletto@gmail.com>
2024-07-09 10:42:04 +0300

    1er commit

--- apps/vente/urls.py
+++ apps/vente/urls.py
@@ -3,5 +3,10 @@ from ..vente import views
 
 urlpatterns = [
     path('icon', views.icon, name='icon'),
-    path('categorie', views.categorie, name='categorie')
+    path('categorie', views.categorie, name='categorie'),
+    path('caisses', views.caisse, name='caisses'),
+    path('commande', views.commande, name='commande'),
+    path('menu', views.menu, name='menu'),
+    path('table', views.table, name='table'),
+    path('historique', views.historique, name='historique'),
 ]
--- apps/vente/views.py
+++ apps/vente/views.py
@@ -7,3 +7,18 @@ def icon(request):
 
 def categorie(request):
     return render(request, "home/categorie.html")
+
+def caisse(request):
+    return render(request, "home/caisse.html")
+
+def commande(request):
+    return render(request, "home/commande.html")
+
+def menu(request):
+    return render(request, "home/menu.html")
+
+def table(request):
+    return render(request, "home/tables.html")
+
+def historique(request):
+    return render(request, "home/historique.html")
